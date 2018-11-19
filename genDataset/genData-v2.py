4417ff7f359bff658879eae7607c700212883291
meiwenPKU <yhmpku@live.cn>
2018-11-19 13:52:45 -0500

    generate a single raw dataset csv file, and every entry contains start, end, and cur time. In this way, we can apply cross-validation on a rolling basis

--- genDataset/genData-v2.py
+++ genDataset/genData-v2.py
@@ -14,9 +14,9 @@ import os, sys, getopt, csv
 def main(argv):
     input_file = ''
     try:
-        opts, args = getopt.getopt(argv,"hi:r:v:",["ifile=", 'trainRange=', 'validationRange'])
+        opts, args = getopt.getopt(argv,"hi:r:",["ifile=", 'trainRange='])
     except getopt.GetoptError:
-        print 'test.py -i <inputfile> -r <tainRange> -v <validationRange>'
+        print 'test.py -i <inputfile> -r <tainRange>'
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
@@ -26,53 +26,22 @@ def main(argv):
             input_file = arg
         elif opt in ("-r", "--timeRange"):
             trainRange = int(arg)
-        elif opt in ("-v", "--validationRange"):
-            validationRange = int(arg)
 
     N_last = 10
     #write the header for the output file
-    v_feature = ['t_last_pkt','Protocol','isEnd']
+    v_feature = ['start', 'end', 'cur', 't_last_pkt','Protocol','isEnd']
     for i in range(0,N_last):
         v_feature.append('p'+str(i)+'-len')
     for i in range(0,N_last-1):
         v_feature.append('i'+str(i)+'-interval')
 
-    train_non_cross_file = input_file.replace('-split-flows.csv', '-dataset-train-non-cross.csv')
-    train_cross_file = input_file.replace('-split-flows.csv', '-dataset-train-cross.csv')
-    validation_non_cross_file = input_file.replace('-split-flows.csv', '-dataset-validation-non-cross.csv')
-    validation_cross_file = input_file.replace('-split-flows.csv', '-dataset-validation-cross.csv')
+    output_file = input_file.replace('-split-flows.csv', '-raw-dataset.csv')
 
-    with open(train_cross_file,'w') as csvfile:
+    with open(output_file,'w') as csvfile:
         writer = csv.DictWriter(csvfile,fieldnames=v_feature)
         writer.writeheader()
-    with open(train_non_cross_file,'w') as csvfile:
-        writer = csv.DictWriter(csvfile,fieldnames=v_feature)
-        writer.writeheader()
-    with open(validation_cross_file,'w') as csvfile:
-        writer = csv.DictWriter(csvfile,fieldnames=v_feature)
-        writer.writeheader()
-    with open(validation_non_cross_file,'w') as csvfile:
-        writer = csv.DictWriter(csvfile,fieldnames=v_feature)
-        writer.writeheader()
-
-    train_cross_set = []
-    train_non_cross_set = []
-    validation_cross_set = []
-    validation_non_cross_set = []
-
-    def appendToSet(start, end, cur, sample):
-        # write the sample to data set
-        if start <= validationRange:
-            if end <= validationRange:
-                train_non_cross_set.append(sample[:])
-            else:
-                if cur <= validationRange:
-                    train_cross_set.append(sample[:])
-                else:
-                    validation_cross_set.append(sample[:])
-        else:
-            validation_non_cross_set.append(sample[:])
     count = 0
+    dataset = []
     for chunk in pd.read_csv(input_file, usecols=['Protocol','arrivals', 'pktLens'], chunksize=1000):
         print "Processing the %d th chunk" % count
         count += 1
@@ -89,7 +58,7 @@ def main(argv):
             if arrivals[-1] <= trainRange:
                 # t_idle can be any value and the label is 1
                 meanInterval = sum(intervals)/len(intervals)
-                sample = [0, entry['Protocol'], 1]
+                sample = [arrivals[0], arrivals[-1], arrivals[-1], 0, entry['Protocol'], 1]
                 for j in range(0,N_last):
                     index = -(N_last-j)
                     if -index > len(pktLens):
@@ -103,8 +72,8 @@ def main(argv):
                     else:
                         sample.append(intervals[index])
                 for i in range(10):
-                    sample[0] = meanInterval*(1<<i)
-                    appendToSet(arrivals[0], arrivals[-1], arrivals[-1], sample)
+                    sample[3] = meanInterval*(1<<i)
+                    dataset.append(sample[:])
 
             # generate data samples for internal flows
             #low = max(1, len(pktLens)-N_last+1)
@@ -112,7 +81,7 @@ def main(argv):
                 if arrivals[i] >= trainRange:
                     continue
                 # generate one sample
-                sample = [intervals[i+1],entry['Protocol'],0]
+                sample = [arrivals[0], arrivals[-1], arrivals[i], intervals[i+1],entry['Protocol'],0]
                 for j in range(0,N_last):
                     index = i+1-(N_last-j)
                     if -index > len(pktLens):
@@ -125,31 +94,15 @@ def main(argv):
                         sample.append(-1)
                     else:
                         sample.append(intervals[index])
-                # write the sample to data set
-                appendToSet(arrivals[0], arrivals[-1], arrivals[i], sample)
+                dataset.append(sample[:])
 
         # write samples to csv files
-        with open(train_cross_file,'ab') as csvfile:
+        with open(output_file,'ab') as csvfile:
             csvwriter = csv.writer(csvfile)
-            for sample in train_cross_set:
+            for sample in dataset:
                 csvwriter.writerow(sample)
-        with open(train_non_cross_file,'ab') as csvfile:
-            csvwriter = csv.writer(csvfile)
-            for sample in train_non_cross_set:
-                csvwriter.writerow(sample)
-        with open(validation_cross_file,'ab') as csvfile:
-            csvwriter = csv.writer(csvfile)
-            for sample in validation_cross_set:
-                csvwriter.writerow(sample)
-        with open(validation_non_cross_file,'ab') as csvfile:
-            csvwriter = csv.writer(csvfile)
-            for sample in validation_non_cross_set:
-                csvwriter.writerow(sample)
-        # clear the four sets
-        train_cross_set = []
-        train_non_cross_set = []
-        validation_cross_set = []
-        validation_non_cross_set = []
+        # clear the sets
+        dataset = []
 
 if __name__ == "__main__":
     main(sys.argv[1:])
